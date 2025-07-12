11effa571bc60696db355e5ebe571f5ce79240e5
yash244466666 <info@yashcodes.com>
2025-07-12 16:33:40 +0600

    init commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+// Function to parse LinkedIn JSON responses
+function parseResponse(response: any): any {
+  return {};
+}
+
+// Example usage
+import ex1 from "./ex-1.json";
+import ex2 from "./ex-2.json";
+import ex3 from "./ex-3.json";
+import ex4 from "./ex-4.json";
+import ex5 from "./ex-5.json";
+import ex6 from "./ex-6.json";
+import ex7 from "./ex-7.json";
+import ex8 from "./ex-8.json";
+import ex9 from "./ex-9.json";
+import ex10 from "./ex-10.json";
+import ex11 from "./ex-11.json";
+
+console.log("Result from ex-1.json:");
+console.log(JSON.stringify(parseResponse(ex1), null, 2));
+console.log("\nResult from ex-2.json:");
+console.log(JSON.stringify(parseResponse(ex2), null, 2));
+console.log("\nResult from ex-3.json:");
+console.log(JSON.stringify(parseResponse(ex3), null, 2));
+console.log("\nResult from ex-4.json:");
+console.log(JSON.stringify(parseResponse(ex4), null, 2));
+console.log("\nResult from ex-5.json:");
+console.log(JSON.stringify(parseResponse(ex5), null, 2));
+console.log("\nResult from ex-6.json:");
+console.log(JSON.stringify(parseResponse(ex6), null, 2));
+console.log("\nResult from ex-7.json:");
+console.log(JSON.stringify(parseResponse(ex7), null, 2));
+console.log("\nResult from ex-8.json:");
+console.log(JSON.stringify(parseResponse(ex8), null, 2));
+console.log("\nResult from ex-9.json:");
+console.log(JSON.stringify(parseResponse(ex9), null, 2));
+console.log("\nResult from ex-10.json:");
+console.log(JSON.stringify(parseResponse(ex10), null, 2));
+console.log("\nResult from ex-11.json:");
+console.log(JSON.stringify(parseResponse(ex11), null, 2));
